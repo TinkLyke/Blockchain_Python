4451c5a9c940f6b12fb604ec2b88d26ec4e67b10
Will Wu <wu23148@hotmail.com>
2021-11-10 19:39:56 -0500

    creating block/hash

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,22 @@
-import uuid 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+from fastecdsa import curve,ecdsa, keys
+import zmq
+import threading
+from flask_mysqldb import MySQL
 
+class Blockchain:
+	def create_genesis_block:
+		curr_time = datetime.datetime.now()
+		block = {}
+		block['index'] = 1
+		block['pre_hash'] = '0000000000'
+		block['nonce'] = 456
+		block['data'] = 'This is the genesis block of blockcahin'
+		block['timestamp'] = curr_time.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-random = uuid.uuid4()
-print(random)

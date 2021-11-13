883f117aeddd6cebe0cc370a3a1eac5dffd2c3b1
Will Wu <wu23148@hotmail.com>
2021-11-13 06:38:27 -0500

    mine/add block into blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,15 +3,15 @@
 
 from datetime import datetime
 from hashlib import sha256
-import json
-from flask import Flask, jsonify, request
-import requests
-from uuid import uuid4
-from urllib.parse import urlparse
-from fastecdsa import curve,ecdsa, keys
-import zmq
-import threading
-from flask_mysqldb import MySQL
+#import json
+#from flask import Flask, jsonify, request
+#import requests
+#from uuid import uuid4
+#from urllib.parse import urlparse
+#from fastecdsa import curve,ecdsa, keys
+#import zmq
+#import threading
+#from flask_mysqldb import MySQL
 
 # Form the hash text
 def form_hash_text(self, *args):
@@ -28,6 +28,7 @@ class Block():
 	nonce = 0
 	prev_hash = "0"*64
 
+	# Initialize new block, overload block number each time
 	def __init__(self, data, number = 0):
 		self.data = data
 		self.number = number
@@ -58,26 +59,61 @@ class Block():
 
 
 class Blockchain():
-	# Set difficulty
+	# Set difficulty - hash with '0000'
 	difficulty = 4
 
+	# Initialize blockchain with a empty chain
 	def __init__(self, chain=[]):
-		self.chain = Blockchain
+		self.chain = chain
 
 	# Adding a block to the chain
 	def add(self, block):
 		self.chain.append({
 				'hash': block.hash(),
-				'previous': block.previous_hash,
+				'previous': block.prev_hash,
 				'number': block.number,
 				'data': block.data,
 				'nonce': block.nonce
 				})
 
+	def mine(self, block):
+		# Try to get the previous hash from the last block of the chain
+		try:
+			block.prev_hash = self.chain[-1].get('hash')
+
+		# If we don't have any
+		except IndexError:
+			pass
+
+		# If the hash of the block starts with '0000', then we add it into block. else, we increase nonce and re-do the hash.
+		while True:
+			if block.hash()[:4] == "0" * 4:
+				self.add(block)
+				break
+			else:
+				block.nonce += 1
+
+
+
 
 def main():
-	block = Block("Hello", 1)
-	print(block)
+
+	# Generating a blockchain and add data into chain
+	new_blockchain = Blockchain()
+	database = ['hello','I want to','sleep','tired']
+
+	block_number = 0
+
+	# Add data starting with no. 1 block
+	for block_data in database:
+		block_number += 1
+		# Create new block with new data
+		new_block = Block(block_data,block_number)
+		
+		new_blockchain.mine(new_block)
+
+	for single_block in new_blockchain.chain:
+		print(single_block)
 
 if __name__ == '__main__':
 	main()

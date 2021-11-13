313b1e638799f5a7fd8aa88d6e85b0071a5be8a9
Will Wu <wu23148@hotmail.com>
2021-11-12 22:57:32 -0500

    adding block to chain

--- blockchain.py
+++ blockchain.py
@@ -34,36 +34,46 @@ class Block():
 
 	# Hash the data
 	def hash(self):
-		hash_txt = form_hash_text(self.prev_hash,self.number,self.data,self.nonce)
+		hash_txt = form_hash_text(
+				self.prev_hash,
+				self.number,
+				self.data,
+				self.nonce
+				)
 
 		hash_func = sha256()
-
-		hash_func.update(hash_txt.encode('utf-8'))
-
+		hash_func.update(hash_txt.encode('utf-8')) 
 		new_hash = hash_func.hexdigest()
 		return new_hash
 
+	# Print block in good format
 	def __str__(self):
-		return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n"
-					%(self.number,self.hash(),self.prev_hash,self.data,self.nonce))
+		return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(
+				self.number,
+				self.hash(),
+				self.prev_hash,
+				self.data,
+				self.nonce
+				))
 
 
 class Blockchain():
+	# Set difficulty
 	difficulty = 4
-#	def create_genesis_block(self):
-#		curr_time = datetime.now()
-#		block = {}
-#		block['index'] = 1
-#		block['prev_hash'] = '0000000000'
-#		block['nonce'] = 456
-#		block['data'] = 'This is the genesis block of blockcahin'
-#		block['timestamp'] = curr_time.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-#		encoded_block = json.dumps(block, sort_keys = True).encode()
+	def __init__(self, chain=[]):
+		self.chain = Blockchain
 
-#		# can change the encryption mode - SHA256
-#		new_hash = hashlib.sha256(encoded_block).hexdigest()
-#		block['new_hash'] = new_hash
+	# Adding a block to the chain
+	def add(self, block):
+		self.chain.append({
+				'hash': block.hash(),
+				'previous': block.previous_hash,
+				'number': block.number,
+				'data': block.data,
+				'nonce': block.nonce
+				})
+
 
 def main():
 	block = Block("Hello", 1)

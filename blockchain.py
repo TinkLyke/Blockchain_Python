5113afd776eed0aa550e3cad8326dcd9f2612d6f
Will Wu <wu23148@hotmail.com>
2021-11-12 22:46:50 -0500

    another approach for creating blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from datetime import datetime
-import hashlib
+from hashlib import sha256
 import json
 from flask import Flask, jsonify, request
 import requests
@@ -10,24 +13,61 @@ import zmq
 import threading
 from flask_mysqldb import MySQL
 
-class Blockchain:
-	def create_genesis_block(self):
-		curr_time = datetime.now()
-		block = {}
-		block['index'] = 1
-		block['pre_hash'] = '0000000000'
-		block['nonce'] = 456
-		block['data'] = 'This is the genesis block of blockcahin'
-		block['timestamp'] = curr_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+# Form the hash text
+def form_hash_text(self, *args):
+	hash_text = ""
+	for arg in args:
+		hash_text += str(arg)
 
-		encoded_block = json.dumps(block, sort_keys = True).encode()
+	return hash_text
 
-		# can change the encryption mode - SHA256
-		new_hash = hashlib.sha256(encoded_block).hexdigest()
-		block['new_hash'] = new_hash
+# Create a block
+class Block():
+	data = ""
+	hash = ""
+	nonce = 0
+	prev_hash = "0"*64
+
+	def __init__(self, data, number = 0):
+		self.data = data
+		self.number = number
+
+	# Hash the data
+	def hash(self):
+		hash_txt = form_hash_text(self.prev_hash,self.number,self.data,self.nonce)
+
+		hash_func = sha256()
+
+		hash_func.update(hash_txt.encode('utf-8'))
+
+		new_hash = hash_func.hexdigest()
+		return new_hash
+
+	def __str__(self):
+		return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n"
+					%(self.number,self.hash(),self.prev_hash,self.data,self.nonce))
 
 
+class Blockchain():
+	difficulty = 4
+#	def create_genesis_block(self):
+#		curr_time = datetime.now()
+#		block = {}
+#		block['index'] = 1
+#		block['prev_hash'] = '0000000000'
+#		block['nonce'] = 456
+#		block['data'] = 'This is the genesis block of blockcahin'
+#		block['timestamp'] = curr_time.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-#new_Blockchain = Blockchain()
+#		encoded_block = json.dumps(block, sort_keys = True).encode()
 
-#new_Blockchain.create_genesis_block()
+#		# can change the encryption mode - SHA256
+#		new_hash = hashlib.sha256(encoded_block).hexdigest()
+#		block['new_hash'] = new_hash
+
+def main():
+	block = Block("Hello", 1)
+	print(block)
+
+if __name__ == '__main__':
+	main()
